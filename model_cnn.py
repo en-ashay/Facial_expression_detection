176189029eb3e0cb191b96e504b58b3071b025f3
Ashay Singh <30903814+ashaycclub@users.noreply.github.com>
2019-10-13 20:08:37 +0530

    Update model_cnn.py
    added architecture for model

--- model_cnn.py
+++ model_cnn.py
@@ -1,57 +1,37 @@
-import numpy as np
-np.random.seed(1)
-import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Activation, Dropout, Flatten
-
-model=Sequential()
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-
-#2nd convolution layer
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-
-#3rd convolution layer
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-
-#4th convolution layer
-model.add(Conv2D(512, (2, 2), activation='relu'))
-model.add(Conv2D(512, (2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(1,1), strides=(2, 2)))
-
-#5th convolution layer
-model.add(Conv2D(1024, (2, 2), activation='relu'))
-model.add(Conv2D(1024, (2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(1,1), strides=(2, 2)))
-
-# converting to 1-d array
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.6))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.4))
-model.add(Dense(256, activation='relu'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(48,48, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.3))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.3))
 
-model.add(Dense(7, activation='softmax')) # check if there were 7 expressions)
+model.add(Conv2D(2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*2*64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+
+model.add(Dense(2*2*2*64, activation='relu'))
 model.add(Dropout(0.4))
+model.add(Dense(2*2*64, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*64, activation='relu'))
+model.add(Dropout(0.5))
 
-model.add(Dense(num_classes, activation='softmax'))
-
-
-# compilation 
-final validation accuracy was ~65.511 %
-epochs 100 
-batch size 64
-
-# Fit the model
+model.add(Dense(num_labels, activation='softmax'))
